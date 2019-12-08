b51631f3f788609bdd222061e4f7d21551db71bb
Android <hyliozqq@gmail.com>
2019-12-08 18:22:33 +0300

    fix isPalindrome method

--- src/main/java/starship/task4/Task4.java
+++ src/main/java/starship/task4/Task4.java
@@ -6,16 +6,15 @@ public class Task4 {
         for (int i = 100; i <= limit; i++) {
             for (int j = 100; j <= limit; j++) {
                 int product = i * j;
-                if (isPalindrome(product) && product > maxPalindrome)
+                if (isPalindrome(Integer.toString(product)) && product > maxPalindrome)
                     maxPalindrome = product;
             }
         }
         return maxPalindrome;
     }
 
-    private static boolean isPalindrome(int number) {
-        String stringFromNumber = Integer.toString(number);
-        if (stringFromNumber.equals(new StringBuilder(stringFromNumber).reverse().toString())) {
+    private static boolean isPalindrome(String str) {
+        if (str.equals(new StringBuilder(str).reverse().toString())) {
             return true;
         }
         return false;
