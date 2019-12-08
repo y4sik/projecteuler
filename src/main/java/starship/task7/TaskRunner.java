8301669d6391399cff33a21d25826447835c41ad
Android <hyliozqq@gmail.com>
2019-12-08 21:53:04 +0300

    fixed variable declaration

--- src/main/java/starship/task7/TaskRunner.java
+++ src/main/java/starship/task7/TaskRunner.java
@@ -1,7 +1,9 @@
 package starship.task7;
 
 public class TaskRunner {
+    private static final int LIMIT = 10_001;
+
     public static void main(String[] args) {
-        System.out.println(Task7.firstPrime(10001));
+        System.out.println(Task7.firstPrime(LIMIT));
     }
 }
