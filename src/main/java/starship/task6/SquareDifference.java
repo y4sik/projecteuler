6492f8cf994eb4bdbec1eee2c0b19ce431383c3e
Android <hyliozqq@gmail.com>
2019-12-11 20:32:24 +0300

    add documentation, change method name

---
+++ src/main/java/starship/task6/SquareDifference.java
@@ -0,0 +1,23 @@
+package starship.task6;
+
+public class SquareDifference {
+
+    /**
+     * Find the difference between the square of the sum of
+     * the natural numbers in range [1, limit] and the sum of the squares
+     *
+     * @param limit the restriction to which the square of the sum and the
+     *              sum of squares is calculated
+     * @return difference between the square of the sum and the sum of the squares
+     */
+
+    public static int findSquareDifference(int limit) {
+        int sumOfSquare = 0;
+        int squareOfSum = 0;
+        for (int i = 1; i <= limit; i++) {
+            sumOfSquare += i * i;
+            squareOfSum += i;
+        }
+        return squareOfSum * squareOfSum - sumOfSquare;
+    }
+}
--- src/main/java/starship/task6/TaskRunner.java
+++ src/main/java/starship/task6/TaskRunner.java
@@ -1,7 +1,10 @@
 package starship.task6;
 
 public class TaskRunner {
+
+    private static final int LIMIT = 100;
+
     public static void main(String[] args) {
-        System.out.println(Task6.sumSquareDifference(100));
+        System.out.println(SquareDifference.findSquareDifference(LIMIT));
     }
 }
