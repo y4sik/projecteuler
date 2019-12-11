ab8ca9f8a8dc79a854d33d015d945af58d2fdcca
student <student@DNTSTUDENT.intexsoft.by>
2019-12-11 13:34:41 +0300

    add documentation

---
+++ src/main/java/starship/task4/Palindrome.java
@@ -0,0 +1,27 @@
+package starship.task4;
+
+public class Palindrome {
+    /**
+     * Finds the largest palindrome made by the product of two numbers, from start to end
+     * A palindromic number reads the same both ways.
+     *
+     * @param start the number at which multiplication begins
+     * @param end   the number at which multiplication ends
+     * @return largest palindrome
+     */
+    public static int maxPalindrome(int start, int end) {
+        int maxPalindrome = -1;
+        for (int i = start; i <= end; i++) {
+            for (int j = start; j <= end; j++) {
+                int product = i * j;
+                if (isPalindrome(Integer.toString(product)) && product > maxPalindrome)
+                    maxPalindrome = product;
+            }
+        }
+        return maxPalindrome;
+    }
+
+    private static boolean isPalindrome(String str) {
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+}
--- src/main/java/starship/task4/TaskRunner.java
+++ src/main/java/starship/task4/TaskRunner.java
@@ -1,7 +1,11 @@
 package starship.task4;
 
 public class TaskRunner {
+
+    private static final int MIN_THREE_DIGIT_NUMBER = 100;
+    private static final int MAX_THREE_DIGIT_NUMBER = 999;
+
     public static void main(String[] args) {
-        System.out.println(Task4.maxPalindrome(999));
+        System.out.println(Palindrome.maxPalindrome(MIN_THREE_DIGIT_NUMBER, MAX_THREE_DIGIT_NUMBER));
     }
 }
