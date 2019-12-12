51fb25738bd9e6258498f8cf44122d8b5c03bc81
student <student@DNTSTUDENT.intexsoft.by>
2019-12-12 15:40:22 +0300

    changed variable names

--- src/main/java/starship/task19/SundayCounter.java
+++ src/main/java/starship/task19/SundayCounter.java
@@ -10,10 +10,10 @@ public class SundayCounter {
      * @param to   year on which the count ends
      * @return the number of Sundays that fell on the first of the month in range [from, to]
      */
-    public static int countSundays(int from, int to) {
+    public static int countSundays(LocalDate from, LocalDate to) {
         int day = 1;
         int count = 0;
-        for (int i = from; i < to; i++) {
+        for (int i = from.getYear(); i < to.getYear(); i++) {
             for (int j = 1; j <= 12; j++) {
                 if (LocalDate.of(i, j, day).getDayOfWeek().getValue() == 1) {
                     count++;
--- src/main/java/starship/task19/TaskRunner.java
+++ src/main/java/starship/task19/TaskRunner.java
@@ -4,10 +4,10 @@ import java.time.LocalDate;
 
 public class TaskRunner {
 
-    private static final int STARTING_YEAR = 1901;
-    private static final int END_YEAR = 2000;
+    private static final LocalDate START_DATE = LocalDate.of(1901, 1, 1);
+    private static final LocalDate END_DATE = LocalDate.of(2000, 12, 31);
 
     public static void main(String[] args) {
-        System.out.println(SundayCounter.countSundays(STARTING_YEAR, END_YEAR));
+        System.out.println(SundayCounter.countSundays(START_DATE, END_DATE));
     }
 }
