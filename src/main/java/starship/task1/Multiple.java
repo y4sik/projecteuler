6e18acd3e3983579f9bf425c2e64e2451c370488
student <student@DNTSTUDENT.intexsoft.by>
2019-12-13 13:04:36 +0300

    fix methodName

--- src/main/java/starship/task1/Multiple.java
+++ src/main/java/starship/task1/Multiple.java
@@ -1,17 +1,18 @@
 package starship.task1;
 
 public class Multiple {
+
     /**
-     * Find the sum of all the numbers below limit, that are multiple to elements in arr
+     * Find the sum of all the numbers below limit, that are multiple to elements in array.
      *
-     * @param arr   elements to test for multiplicity
+     * @param array elements to test for multiplicity
      * @param limit the restriction for finding multiples
      * @return the sum of all the multiples elements
      */
-    public static int findMultiples(int limit, int... arr) {
+    public static int findMultipleNumbersToArrayElements(int limit, int... array) {
         int sum = 0;
         for (int i = 1; i < limit; i++) {
-            for (int value : arr) {
+            for (int value : array) {
                 if (i % value == 0) {
                     sum += i;
                     break;
--- src/main/java/starship/task1/TaskRunner.java
+++ src/main/java/starship/task1/TaskRunner.java
@@ -2,10 +2,10 @@ package starship.task1;
 
 public class TaskRunner {
     private static final int LIMIT = 1000;
-    private static final int A = 3;
-    private static final int B = 5;
+    private static final int NUMBER_A = 3;
+    private static final int NUMBER_B = 5;
 
     public static void main(String[] args) {
-        System.out.println(Multiple.findMultiples(LIMIT, A, B));
+        System.out.println(Multiple.findMultipleNumbersToArrayElements(LIMIT, NUMBER_A, NUMBER_B));
     }
 }
