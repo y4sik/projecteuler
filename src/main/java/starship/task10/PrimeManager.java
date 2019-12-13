51103d5ef607644aa32f0f06b72ad022b84d54e4
student <student@DNTSTUDENT.intexsoft.by>
2019-12-13 17:04:40 +0300

    change variable names

--- src/main/java/starship/task10/PrimeManager.java
+++ src/main/java/starship/task10/PrimeManager.java
@@ -8,16 +8,16 @@ public class PrimeManager {
      * @param limit the number to which prime numbers must be found
      * @return sum of primes below limit
      */
-    public static long findSumOfPrimes(int limit) {
+    public static long findSumPrimes(int limit) {
         long sum = 0;
-        for (int i = 0; i < limit; i++) {
-            if (isPrime(i)) {
-                sum += i;
+        for (int index = 0; index < limit; index++) {
+            if (isPrime(index)) {
+                sum += index;
             }
         }
         return sum;
     }
-    
+
     /**
      * Check if number is prime number
      *
@@ -25,23 +25,17 @@ public class PrimeManager {
      * @return true if number is prime
      */
     private static boolean isPrime(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException("Negative number");
+        if (number == 2) {
+            return true;
         }
-        if (number == 0 || number == 1) {
+        if (number < 0 || number == 1 || number % 2 == 0) {
             return false;
-        } else if (number == 2) {
-            return true;
-        } else {
-            if (number % 2 == 0) {
-                return false;
-            }
-            int end = (int) Math.ceil(Math.sqrt(number));
-            for (int i = 3; i <= end; i += 2) {
-                if (number % i == 0)
-                    return false;
-            }
-            return true;
         }
+        int end = (int) Math.ceil(Math.sqrt(number));
+        for (int index = 3; index <= end; index += 2) {
+            if (number % index == 0)
+                return false;
+        }
+        return true;
     }
 }
--- src/main/java/starship/task10/TaskRunner.java
+++ src/main/java/starship/task10/TaskRunner.java
@@ -4,6 +4,6 @@ public class TaskRunner {
     private static final int LIMIT = 2_000_000;
 
     public static void main(String[] args) {
-        System.out.println(PrimeManager.findSumOfPrimes(LIMIT));
+        System.out.println(PrimeManager.findSumPrimes(LIMIT));
     }
 }
