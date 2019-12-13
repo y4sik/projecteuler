9866dab1027ce27d6b25d9bcc2a33777e7d1e4cb
student <student@DNTSTUDENT.intexsoft.by>
2019-12-13 15:48:22 +0300

    change method name

--- src/main/java/starship/task1/MultipleManager.java
+++ src/main/java/starship/task1/MultipleManager.java
@@ -5,11 +5,11 @@ public class MultipleManager {
     /**
      * Find the sum of all the numbers below limit, that are multiple to elements in array.
      *
-     * @param array elements to test for multiplicity
+     * @param array contains elements for checking for multiplicity
      * @param limit the restriction for finding multiples
      * @return the sum of all the multiples elements
      */
-    public static int findMultipleNumbersToArrayElements(int limit, int... array) {
+    public static int findSumOfNumbersMultipleToArrayElements(int limit, int... array) {
         int sum = 0;
         for (int i = 1; i < limit; i++) {
             for (int value : array) {
