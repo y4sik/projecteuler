67a0c9d2404f84fe055191e3bff3861bb2fe7ec0
student <student@DNTSTUDENT.intexsoft.by>
2019-12-13 16:25:09 +0300

    change method name

--- src/main/java/starship/task6/SquareDifference.java
+++ src/main/java/starship/task6/SquareDifference.java
@@ -10,14 +10,13 @@ public class SquareDifference {
      *              sum of squares is calculated
      * @return difference between the square of the sum and the sum of the squares
      */
-
-    public static int findSquareDifference(int limit) {
-        int sumOfSquare = 0;
-        int squareOfSum = 0;
+    public static int findDifferenceBetweenSquaresSumAndSumSquare(int limit) {
+        int sumSquare = 0;
+        int squaresSum = 0;
         for (int i = 1; i <= limit; i++) {
-            sumOfSquare += i * i;
-            squareOfSum += i;
+            sumSquare += i * i;
+            squaresSum += i;
         }
-        return squareOfSum * squareOfSum - sumOfSquare;
+        return squaresSum * squaresSum - sumSquare;
     }
 }
--- src/main/java/starship/task6/TaskRunner.java
+++ src/main/java/starship/task6/TaskRunner.java
@@ -5,6 +5,6 @@ public class TaskRunner {
     private static final int LIMIT = 100;
 
     public static void main(String[] args) {
-        System.out.println(SquareDifference.findSquareDifference(LIMIT));
+        System.out.println(SquareDifference.findDifferenceBetweenSquaresSumAndSumSquare(LIMIT));
     }
 }
