3cc781ea49c5c7e26369b854ddaf9fa33183c1fd
student <student@DNTSTUDENT.intexsoft.by>
2019-12-16 11:34:24 +0300

    add documentation, change method name

--- src/main/java/starship/task12/TaskRunner.java
+++ src/main/java/starship/task12/TaskRunner.java
@@ -1,9 +1,9 @@
 package starship.task12;
 
 public class TaskRunner {
-    private static final int DIVISORS_COUNT = 500;
+    private static final int COUNT_DIVIDERS = 500;
 
     public static void main(String[] args) {
-        System.out.println(Task12.highlyDivisibleTriangularNumber(DIVISORS_COUNT));
+        System.out.println(TriangularNumber.findFirstTriangularNumberDivisibleByDividersSequence(COUNT_DIVIDERS));
     }
 }
