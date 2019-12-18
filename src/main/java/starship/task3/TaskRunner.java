7413fe493b30cbe5d6377d62c26b9174d27391d4
admin <yaskelevich.andrey@gmail.com>
2019-12-18 10:59:47 +0300

    change number value

--- src/main/java/starship/task3/TaskRunner.java
+++ src/main/java/starship/task3/TaskRunner.java
@@ -3,7 +3,7 @@ package starship.task3;
 import java.util.List;
 
 public class TaskRunner {
-    private static final long NUMBER = 28;
+    private static final long NUMBER = 600851475143L;
 
     public static void main(String[] args) {
         List<Long> primeFactorList = PrimeFactor.findPrimeFactors(NUMBER);
