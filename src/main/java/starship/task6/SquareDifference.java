2ef2f786c8433b35094ace95fcb925e667a43e1a
y4sik <yaskelevich.andrey@gmail.com>
2019-12-20 16:21:44 +0300

    chenge variable name

--- src/main/java/starship/task6/SquareDifference.java
+++ src/main/java/starship/task6/SquareDifference.java
@@ -6,16 +6,16 @@ public class SquareDifference {
      * Find the difference between the square of the sum and
      * the sum of the squares of the natural numbers, in range [1, limit].
      *
-     * @param limit the restriction to which the square of the sum and the
-     *              sum of squares is calculated
+     * @param sequenceLimit the restriction to which the square of the sum and the
+     *                      sum of squares is calculated
      * @return difference between the square of the sum and the sum of the squares
      */
-    public static int findDifferenceBetweenSquaresSumAndSumSquare(int limit) {
+    public static int findDifferenceBetweenSquaresSumAndSumSquare(int sequenceLimit) {
         int sumSquare = 0;
         int squaresSum = 0;
-        for (int i = 1; i <= limit; i++) {
-            sumSquare += i * i;
-            squaresSum += i;
+        for (int number = 1; number <= sequenceLimit; number++) {
+            sumSquare += number * number;
+            squaresSum += number;
         }
         return squaresSum * squaresSum - sumSquare;
     }
--- src/main/java/starship/task6/TaskRunner.java
+++ src/main/java/starship/task6/TaskRunner.java
@@ -2,9 +2,9 @@ package starship.task6;
 
 public class TaskRunner {
 
-    private static final int LIMIT = 100;
+    private static final int SEQUENCE_LIMIT = 100;
 
     public static void main(String[] args) {
-        System.out.println(SquareDifference.findDifferenceBetweenSquaresSumAndSumSquare(LIMIT));
+        System.out.println(SquareDifference.findDifferenceBetweenSquaresSumAndSumSquare(SEQUENCE_LIMIT));
     }
 }
