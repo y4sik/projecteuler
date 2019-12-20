228381fb9eae400ee202f7f64ab13f9b433e9bb5
y4sik <yaskelevich.andrey@gmail.com>
2019-12-20 17:14:01 +0300

    change variable names

--- src/main/java/starship/task7/PrimeManager.java
+++ src/main/java/starship/task7/PrimeManager.java
@@ -5,34 +5,28 @@ public class PrimeManager {
     /**
      * Finds n-th prime number (n > 0).
      *
-     * @param ordinalNumber the ordinal number on which the prime number is searched
+     * @param ordinalPrimeNumber the ordinal number on which the prime number is searched
      * @return n-th prime number
      */
-    public static int findOrdinalNumberOfPrime(int ordinalNumber) {
-        if (ordinalNumber < 0) {
-            throw new IllegalArgumentException("Negative ordinal number");
-        }
-        int count = 0;
-        int i = 0;
-        while (count < ordinalNumber) {
-            i++;
-            if (isPrime(i)) {
-                count += 1;
+    public static int findPrimeByOrdinalNumber(int ordinalPrimeNumber) {
+        int currentOrdinalPrimeNumber = 0;
+        int number = 0;
+        while (currentOrdinalPrimeNumber < ordinalPrimeNumber) {
+            number++;
+            if (isPrime(number)) {
+                currentOrdinalPrimeNumber += 1;
             }
         }
-        return i;
+        return number;
     }
 
     /**
-     * Check if number is prime number
+     * Check if number is prime.
      *
      * @param number the number to be tested on prime
      * @return true if number is prime
      */
     private static boolean isPrime(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException("Negative number");
-        }
         if (number == 0 || number == 1) {
             return false;
         } else if (number == 2) {
@@ -41,9 +35,9 @@ public class PrimeManager {
             if (number % 2 == 0) {
                 return false;
             }
-            int end = (int) Math.ceil(Math.sqrt(number));
-            for (int i = 3; i <= end; i += 2) {
-                if (number % i == 0)
+            int lastDivisor = (int) Math.ceil(Math.sqrt(number));
+            for (int divisor = 3; divisor <= lastDivisor; divisor += 2) {
+                if (number % divisor == 0)
                     return false;
             }
             return true;
--- src/main/java/starship/task7/TaskRunner.java
+++ src/main/java/starship/task7/TaskRunner.java
@@ -1,9 +1,9 @@
 package starship.task7;
 
 public class TaskRunner {
-    private static final int NUMBER_OF_PRIME_NUMBER = 10_001;
+    private static final int NUMBER_OF_PRIME = 10_001;
 
     public static void main(String[] args) {
-        System.out.println(PrimeManager.findOrdinalNumberOfPrime(NUMBER_OF_PRIME_NUMBER));
+        System.out.println(PrimeManager.findPrimeByOrdinalNumber(NUMBER_OF_PRIME));
     }
 }
