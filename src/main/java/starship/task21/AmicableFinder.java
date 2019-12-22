493a78a127f5bbfba7d12a471ebfb3dc8d156ab9
Android <hyliozqq@gmail.com>
2019-12-22 17:52:44 +0300

    change variable names

--- src/main/java/starship/task21/AmicableFinder.java
+++ src/main/java/starship/task21/AmicableFinder.java
@@ -3,7 +3,7 @@ package starship.task21;
 public class AmicableFinder {
     /**
      * <p>
-     * Find the sum of all the amicable numbers under limit.
+     * Find the sum of all the amicable numbers before [maxNumber].
      * </p>
      * Let d(n) be defined as the sum of proper divisors of
      * n (numbers less than n which divide evenly into n).
@@ -11,29 +11,29 @@ public class AmicableFinder {
      * are an amicable pair and each of a and b are called
      * amicable numbers.
      *
-     * @param limit the limit for finding amicable numbers
+     * @param maxNumber the limit for finding amicable numbers
      * @return sum of amicable numbers below limit
      */
-    public static int sumOfAmicable(int limit) {
-        int sum = 0;
-        for (int i = 0; i < limit; i++) {
-            if (isAmicable(i)) {
-                sum += i;
+    public static int sumOfAmicable(int maxNumber) {
+        int sumAmicableNumbers = 0;
+        for (int number = 0; number < maxNumber; number++) {
+            if (isAmicable(number)) {
+                sumAmicableNumbers += number;
             }
         }
-        return sum;
+        return sumAmicableNumbers;
     }
 
     /**
-     * Check if n is amicable number.
+     * Check if number is amicable.
      *
-     * @param n number to check for amicability
+     * @param number the number to check for amicability
      * @return true if number is amicable
      */
-    private static boolean isAmicable(int n) {
-        int m = sumOfDividers(n);
-        if (n != m) {
-            return n == sumOfDividers(m);
+    private static boolean isAmicable(int number) {
+        int dividersSum = findDividersSum(number);
+        if (number != dividersSum) {
+            return number == findDividersSum(dividersSum);
         }
         return false;
     }
@@ -44,13 +44,13 @@ public class AmicableFinder {
      * @param number the number for which the divisors are found
      * @return sum of dividers
      */
-    private static int sumOfDividers(int number) {
-        int sum = 0;
-        for (int i = 1; i < number; i++) {
-            if (number % i == 0) {
-                sum += i;
+    private static int findDividersSum(int number) {
+        int dividersSum = 0;
+        for (int divider = 1; divider < number; divider++) {
+            if (number % divider == 0) {
+                dividersSum += divider;
             }
         }
-        return sum;
+        return dividersSum;
     }
 }
--- src/main/java/starship/task21/TaskRunner.java
+++ src/main/java/starship/task21/TaskRunner.java
@@ -1,12 +1,10 @@
 package starship.task21;
 
-import java.util.Arrays;
-
 public class TaskRunner {
 
-    private static final int LIMIT = 10_000;
+    private static final int MAX_NUMBER = 10_000;
 
     public static void main(String[] args) {
-        System.out.println(AmicableFinder.sumOfAmicable(LIMIT));
+        System.out.println(AmicableFinder.sumOfAmicable(MAX_NUMBER));
     }
 }
