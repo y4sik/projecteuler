2ffd7af92463f3777f590606425a428e220627eb
y4sik <yaskelevich.andrey@gmail.com>
2019-12-31 13:39:24 +0300

    add task description

--- src/main/java/starship/task6/TaskRunner.java
+++ src/main/java/starship/task6/TaskRunner.java
@@ -4,6 +4,17 @@ public class TaskRunner {
 
     private static final int SEQUENCE_LIMIT = 100;
 
+    /**
+     * The sum of the squares of the first ten natural numbers is,
+     * <p>
+     * 12 + 22 + ... + 102 = 385
+     * The square of the sum of the first ten natural numbers is,
+     * <p>
+     * (1 + 2 + ... + 10)2 = 552 = 3025
+     * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+     * <p>
+     * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+     */
     public static void main(String[] args) {
         System.out.println(SquareDifference.findDifferenceBetweenSquaresSumAndSumSquare(SEQUENCE_LIMIT));
     }
