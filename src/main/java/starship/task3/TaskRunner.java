2e50724e2217aadd9d72a5a1631b28e18b49f4d9
y4sik <yaskelevich.andrey@gmail.com>
2019-12-31 13:43:35 +0300

    add task description

--- src/main/java/starship/task3/TaskRunner.java
+++ src/main/java/starship/task3/TaskRunner.java
@@ -5,6 +5,12 @@ import java.util.List;
 public class TaskRunner {
     private static final long NUMBER = 600851475143L;
 
+    /**
+     * A palindromic number reads the same both ways. The largest palindrome
+     * made from the product of two 2-digit numbers is 9009 = 91 × 99.
+     * <p>
+     * Find the largest palindrome made from the product of two 3-digit numbers.
+     */
     public static void main(String[] args) {
         List<Long> primeFactorList = PrimeFactor.findPrimeFactors(NUMBER);
         System.out.println(primeFactorList.get(primeFactorList.size() - 1));
