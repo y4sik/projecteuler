68970e02eba6a3f8549b3bb8515f08e425e3092f
y4sik <yaskelevich.andrey@gmail.com>
2019-12-31 13:31:30 +0300

    add task description

--- src/main/java/starship/task9/TaskRunner.java
+++ src/main/java/starship/task9/TaskRunner.java
@@ -4,6 +4,16 @@ public class TaskRunner {
 
     private static final int NUMBER = 1000;
 
+    /**
+     * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+     * <p>
+     * a2 + b2 = c2
+     * For example, 32 + 42 = 9 + 16 = 25 = 52.
+     * <p>
+     * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+     * Find the product abc.
+     */
+
     public static void main(String[] args) {
         System.out.println(PythagoreanTriplet.findPythagoreanTripletProduct(NUMBER));
     }
