63daf33173dbf6278200977e76648730322b85cc
Android <hyliozqq@gmail.com>
2020-01-02 17:22:44 +0300

    add tests for 7th task

---
+++ src/test/java/starship/task7/PrimeManagerTest.java
@@ -0,0 +1,62 @@
+package starship.task7;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeManagerTest {
+
+    private static HashMap<Integer, Integer> ordinalPrimeMap = new HashMap<>();
+    private static int[] testNumbers = new int[6];
+
+    @BeforeAll
+    static void setUp() {
+        ordinalPrimeMap.put(1, 2);
+        ordinalPrimeMap.put(3, 5);
+        ordinalPrimeMap.put(5, 11);
+        ordinalPrimeMap.put(0, 0);
+        testNumbers[0] = 1;
+        testNumbers[1] = 3;
+        testNumbers[2] = 5;
+        testNumbers[3] = 0;
+        testNumbers[4] = -1;
+        testNumbers[5] = -5;
+    }
+
+
+    @Test
+    void findPrimeByOrdinalNumber() {
+        int expected = PrimeManager.findPrimeByOrdinalNumber(testNumbers[0]);
+        int actual = ordinalPrimeMap.get(testNumbers[0]);
+
+        assertEquals(expected, actual);
+
+        expected = PrimeManager.findPrimeByOrdinalNumber(testNumbers[1]);
+        actual = ordinalPrimeMap.get(testNumbers[1]);
+
+        assertEquals(expected, actual);
+
+        expected = PrimeManager.findPrimeByOrdinalNumber(testNumbers[2]);
+        actual = ordinalPrimeMap.get(testNumbers[2]);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findPrimeByOrdinalNumberBoundaryValues() {
+        int expected = PrimeManager.findPrimeByOrdinalNumber(testNumbers[3]);
+        int actual = ordinalPrimeMap.get(testNumbers[3]);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findPrimeByOrdinalNumberIllegalArguments() {
+        assertThrows(IllegalArgumentException.class, () -> PrimeManager.findPrimeByOrdinalNumber(testNumbers[4]));
+        assertThrows(IllegalArgumentException.class, () -> PrimeManager.findPrimeByOrdinalNumber(testNumbers[5]));
+    }
+}
