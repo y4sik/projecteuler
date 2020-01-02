44ae5d834eb82dfa9e339596dc6134ad8ecbb354
Android <hyliozqq@gmail.com>
2020-01-02 22:01:57 +0300

    change variable name

--- src/test/java/starship/task9/PythagoreanTripletTest.java
+++ src/test/java/starship/task9/PythagoreanTripletTest.java
@@ -1,7 +1,6 @@
 package starship.task9;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -10,54 +9,55 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PythagoreanTripletTest {
 
-    private static HashMap<Integer, Integer> ordinalPrimeMap = new HashMap<>();
+    private static HashMap<Integer, Integer> pythagoreanTripletMap = new HashMap<>();
     private static int[] testNumbers = new int[6];
 
     @BeforeAll
     static void setUp() {
-        ordinalPrimeMap.put(1000, 31875000);
-        ordinalPrimeMap.put(10_000, 1810228928);
-        ordinalPrimeMap.put(100, null);
-        ordinalPrimeMap.put(25, null);
         testNumbers[0] = 1000;
         testNumbers[1] = 10_000;
         testNumbers[2] = 100;
         testNumbers[3] = 25;
         testNumbers[4] = -1;
         testNumbers[5] = -5;
-    }
+        pythagoreanTripletMap.put(testNumbers[0], 31875000);
+        pythagoreanTripletMap.put(testNumbers[1], 1810228928);
+        pythagoreanTripletMap.put(testNumbers[2], null);
+        pythagoreanTripletMap.put(testNumbers[3], null);
 
+    }
 
     @Test
     void findPythagoreanTripletProduct() {
         Integer expected = PythagoreanTriplet.findPythagoreanTripletProduct(testNumbers[0]);
-        Integer actual = ordinalPrimeMap.get(testNumbers[0]);
+        Integer actual = pythagoreanTripletMap.get(testNumbers[0]);
 
         assertEquals(expected, actual);
 
         expected = PythagoreanTriplet.findPythagoreanTripletProduct(testNumbers[1]);
-        actual = ordinalPrimeMap.get(testNumbers[1]);
+        actual = pythagoreanTripletMap.get(testNumbers[1]);
 
         assertEquals(expected, actual);
-
     }
 
     @Test
     void findPythagoreanTripletProductNotExist() {
         Integer expected = PythagoreanTriplet.findPythagoreanTripletProduct(testNumbers[2]);
-        Integer actual = ordinalPrimeMap.get(testNumbers[2]);
+        Integer actual = pythagoreanTripletMap.get(testNumbers[2]);
 
         assertEquals(expected, actual);
 
         expected = PythagoreanTriplet.findPythagoreanTripletProduct(testNumbers[3]);
-        actual = ordinalPrimeMap.get(testNumbers[3]);
+        actual = pythagoreanTripletMap.get(testNumbers[3]);
 
         assertEquals(expected, actual);
     }
 
     @Test
     void findPythagoreanTripletProductIllegalArguments() {
-        assertThrows(IllegalArgumentException.class, () -> PythagoreanTriplet.findPythagoreanTripletProduct(testNumbers[4]));
-        assertThrows(IllegalArgumentException.class, () -> PythagoreanTriplet.findPythagoreanTripletProduct(testNumbers[5]));
+        assertThrows(IllegalArgumentException.class,
+                () -> PythagoreanTriplet.findPythagoreanTripletProduct(testNumbers[4]));
+        assertThrows(IllegalArgumentException.class,
+                () -> PythagoreanTriplet.findPythagoreanTripletProduct(testNumbers[5]));
     }
 }
