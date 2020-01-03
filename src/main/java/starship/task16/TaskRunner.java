38f2b5baadaa8f7c219bc20bb2f2a712fe6c5893
y4sik <yaskelevich.andrey@gmail.com>
2020-01-03 12:24:25 +0300

    add task description

--- src/main/java/starship/task16/TaskRunner.java
+++ src/main/java/starship/task16/TaskRunner.java
@@ -5,6 +5,11 @@ public class TaskRunner {
     private static final int NUMBER = 2;
     private static final int DEGREE = 1000;
 
+    /**
+     * 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+     * <p>
+     * What is the sum of the digits of the number 21000?
+     */
     public static void main(String[] args) {
         System.out.println(DigitSum.findDigitSumNumberInDegree(NUMBER, DEGREE));
     }
