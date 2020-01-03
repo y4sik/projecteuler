b2ffebbbbf50123cf6bb53245861866a2d106a5e
y4sik <yaskelevich.andrey@gmail.com>
2020-01-03 12:34:28 +0300

    add task description

--- src/main/java/starship/task20/TaskRunner.java
+++ src/main/java/starship/task20/TaskRunner.java
@@ -13,6 +13,14 @@ public class TaskRunner {
 
     private static final int ITERATIONS_COUNT = 50;
 
+    /**
+     * n! means n × (n − 1) × ... × 3 × 2 × 1
+     * <p>
+     * For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+     * and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+     * <p>
+     * Find the sum of the digits in the number 100!
+     */
     public static void main(String[] args) {
 
         showAlgorithmRunningTime();
