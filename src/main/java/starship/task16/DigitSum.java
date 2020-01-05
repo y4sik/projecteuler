348525e89d20d2a5409fe7c256abfe0ac2cad737
Android <hyliozqq@gmail.com>
2020-01-05 17:51:34 +0300

    fix task description, remove constants

--- src/main/java/starship/task16/DigitSum.java
+++ src/main/java/starship/task16/DigitSum.java
@@ -7,11 +7,11 @@ public class DigitSum {
      * Find sum of digits of number in a degree.
      *
      * @param number the number raised to a degree
-     * @param degree the degree to which the number is raised
+     * @param numberDegree the degree to which the number is raised
      * @return sum of digits of number in a degree
      */
-    public static int findDigitSumNumberInDegree(int number, int degree) {
-        String numericString = BigInteger.valueOf(number).pow(degree).toString();
+    public static int findDigitSumNumberInDegree(int number, int numberDegree) {
+        String numericString = BigInteger.valueOf(number).pow(numberDegree).toString();
         int digitSum = 0;
         for (int digitIndex = 0; digitIndex < numericString.length(); digitIndex++) {
             digitSum += Character.getNumericValue(numericString.charAt(digitIndex));
--- src/main/java/starship/task16/TaskRunner.java
+++ src/main/java/starship/task16/TaskRunner.java
@@ -1,16 +1,15 @@
 package starship.task16;
 
 public class TaskRunner {
-
-    private static final int NUMBER = 2;
-    private static final int DEGREE = 1000;
-
+    
     /**
-     * 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+     * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
      * <p>
-     * What is the sum of the digits of the number 21000?
+     * What is the sum of the digits of the number 2^1000?
      */
     public static void main(String[] args) {
-        System.out.println(DigitSum.findDigitSumNumberInDegree(NUMBER, DEGREE));
+        int number = 2;
+        int numberDegree = 1000;
+        System.out.println(DigitSum.findDigitSumNumberInDegree(number, numberDegree));
     }
 }
