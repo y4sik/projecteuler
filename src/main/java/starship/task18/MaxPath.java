9db3d5ba595fdfb0b38c9c1a6c3e0ca110b0c67e
Android <hyliozqq@gmail.com>
2020-01-05 19:25:07 +0300

    made the array immutable

--- src/main/java/starship/task18/MaxPath.java
+++ src/main/java/starship/task18/MaxPath.java
@@ -1,5 +1,8 @@
 package starship.task18;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class MaxPath {
     /**
      * Find the maximum sum from top to bottom of the triangle.
@@ -9,11 +12,21 @@ public class MaxPath {
      * @return the maximum sum from top to bottom of the arr
      */
     public static int maxSumFromTopToBottom(int[][] triangleArray) {
-        for (int rowIndex = triangleArray.length - 2; rowIndex >= 0; rowIndex--) {
-            for (int columnIndex = 0; columnIndex < triangleArray[rowIndex].length; columnIndex++)
-                triangleArray[rowIndex][columnIndex] +=
-                        Math.max(triangleArray[rowIndex + 1][columnIndex], triangleArray[rowIndex + 1][columnIndex + 1]);
+        int[][] triangleArrayCopy = copyArray(triangleArray);
+        for (int rowIndex = triangleArrayCopy.length - 2; rowIndex >= 0; rowIndex--) {
+            for (int columnIndex = 0; columnIndex < triangleArrayCopy[rowIndex].length; columnIndex++) {
+                triangleArrayCopy[rowIndex][columnIndex] +=
+                        Math.max(triangleArrayCopy[rowIndex + 1][columnIndex], triangleArrayCopy[rowIndex + 1][columnIndex + 1]);
+            }
         }
-        return triangleArray[0][0];
+        return triangleArrayCopy[0][0];
+    }
+
+    private static int[][] copyArray(int[][] array) {
+        int[][] copiedArray = new int[array.length][];
+        for (int rowIndex = 0; rowIndex < array.length; rowIndex++) {
+            copiedArray[rowIndex] = Arrays.copyOf(array[rowIndex], array[rowIndex].length);
+        }
+        return copiedArray;
     }
 }
